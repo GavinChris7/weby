a68e30d49637e3ec522f95cc2f5502b51b8b1432
GavinChris7 <gavchris.37@gmail.com>
2020-07-30 16:45:31 +0530

    Checklog Modified

--- src/index.js
+++ src/index.js
@@ -1,17 +1,70 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class CountCharacters extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: "",
+      counter: 10,
+    };
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  onMessageChange(text) {
+    this.setState({
+      message: "Message has " + text.length + " number of Characters.",
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Welcome to Count Characters Component :</h2>
+        <p>
+          <label>
+            {" "}
+            Enter Messsage :{" "}
+            <input
+              type="text"
+              onChange={(e) => this.onMessageChange(e.target.value)}
+            ></input>{" "}
+          </label>
+        </p>
+        <p>
+          <label>{this.state.message}</label>
+        </p>
+        <p>
+          <label>{this.state.counter}</label>
+        </p>
+      </div>
+    );
+  }
+}
+class Employee extends React.Component {
+  state = { counter: 0 };
+  addEmployee = () => {
+    this.setState({ counter: this.state.counter + 1 });
+    //this.counter = this.counter + 1;
+    //alert("Adding a New Employee");
+    //alert("Add Employee Nutton is Clicked " + this.counter + " times");
+  };
+  render() {
+    return (
+      <div>
+        <h2>Welcome to Employee Component :</h2>
+        <p>
+          <button onClick={this.addEmployee}>Add Employee</button>
+        </p>
+        <p>
+          <label>
+            Add Employee Button is Clicked <b>{this.state.counter}</b> times.
+          </label>
+        </p>
+      </div>
+    );
+  }
+}
+
+const element = <CountCharacters></CountCharacters>; //<Employee></Employee>;
+
+ReactDOM.render(element, document.getElementById("root"));
