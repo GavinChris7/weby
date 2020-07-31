ec975907ad1bdd4f1a2eaf8ae1bce8ee8f49aa6e
GavinChris7 <gavchris.37@gmail.com>
2020-08-01 02:33:11 +0530

    mod index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 import Amplify from "aws-amplify";
 import config from "./aws-exports";
+
 Amplify.configure(config);
+
+const element = <h1 className="testClass">Welcome to React Programming...</h1>;
+
+ReactDOM.render(element, document.getElementById("root"));
+
+const newElement = (
+  <h1 className="testClass">Understanding React Programming</h1>
+);
+
+ReactDOM.render(newElement, document.getElementById("app"));
 
 ReactDOM.render(
   <React.StrictMode>
