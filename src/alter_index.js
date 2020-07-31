8903239f35bf889542f5a1671d66023b37e985bf
GavinChris7 <gavchris.37@gmail.com>
2020-08-01 02:12:01 +0530

    auth added

--- src/alter_index.js
+++ src/alter_index.js
@@ -240,3 +240,32 @@ ReactDOM.render(element, document.getElementById("root"));
 
 
 */
+
+/*6
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React from Chris!!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+*/
