3205ab0a007d1b58bca8d4034bc2eba8a009ae13
GavinChris7 <gavchris.37@gmail.com>
2020-08-01 02:08:54 +0530

    AWS-Amplify

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Amplify from "aws-amplify";
+import config from "./aws-exports";
+Amplify.configure(config);
 
 class CountCharacters extends React.Component {
   constructor(props) {
